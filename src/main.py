ddf1fdcfbaa97b1a15e38c4023625d84536c9834
Yan Figueiredo <yancfm@gmail.com>
2021-07-15 16:55:00 -0300

    blank

--- src/main.py
+++ src/main.py
@@ -18,8 +18,6 @@ bot = commands.Bot(command_prefix='.', intents=intents)
 bot.add_command(creation.create)
 
 
-
-
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
