98db350dfbf4e6290c987914d5c4e25f92da79c7
Yan Figueiredo <yancfm@gmail.com>
2021-07-22 17:09:45 -0300

    load feedback

--- cogs/loadmanager.py
+++ cogs/loadmanager.py
@@ -17,6 +17,10 @@ import os
 from dotenv import dotenv_values
 ENV = dotenv_values(os.path.dirname(os.path.abspath(__file__)) + "/../.env")
 
+def isOwner(ctx):
+    return ctx.author.id == 366342872892440586
+
+
 class LoadManager(commands.Cog):
     """
         Manager is useful to create and delete roles.
@@ -38,19 +42,47 @@ class LoadManager(commands.Cog):
     
     
     @commands.command()
+    @commands.check(isOwner)
     async def load(self, ctx, extension: str):
         
         await ctx.message.delete(delay = self.delete_user_message)
         
         self.client.load_extension(f"cogs.{extension}")
+        
+        embedmsg = embed.createEmbed(title="Extensão carregada com sucesso!", 
+            description= f"A extensão '{extension}' foi carregada!.",
+            color=rgb_to_int((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))),
+            fields=[
+            ],
+            img="https://cdn.discordapp.com/emojis/767196592490807347.png?v=1")
+
+        # Send that shit
+        await after.send(embed=embedmsg, delete_after = self.delete_system_message)
+
+
+    @load.error
+    async def owner_error(error, ctx):
+        if isinstance(error, commands.MissingPermissions):
+            ctx.send("Você não tem permissão para executar o comando!")
 
     @commands.command()
     async def unload(self, ctx, extension: str):
         
         await ctx.message.delete(delay = self.delete_user_message)
-
         
         self.client.unload_extension(f"cogs.{extension}")
+        
+        embedmsg = embed.createEmbed(title="Extensão descarregada com sucesso!", 
+            description= f"A extensão '{extension}' foi descarregada!.",
+            color=rgb_to_int((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))),
+            fields=[
+            ],
+            img="https://cdn.discordapp.com/emojis/766457934905081887.png?v=1")
+
+        # Send that shit
+        await after.send(embed=embedmsg, delete_after = self.delete_system_message)
+
+        
     
     @commands.command()
     async def reload(self, ctx, extension: str):
@@ -59,19 +91,48 @@ class LoadManager(commands.Cog):
         
         self.client.reload_extension(f"cogs.{extension}")
 
+        await ctx.message.delete(delay = self.delete_user_message)
+        
+        embedmsg = embed.createEmbed(title="Extensão recarregada com sucesso!", 
+            description= f"A extensão '{extension}' foi recarregada!.",
+            color=rgb_to_int((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))),
+            fields=[
+            ],
+            img="https://cdn.discordapp.com/emojis/767196592490807347.png?v=1")
+
+        # Send that shit
+        await after.send(embed=embedmsg, delete_after = self.delete_system_message)
+
     
     @commands.command()
     async def reloadall(self, ctx):
         with open(os.path.dirname(os.path.abspath(__file__))  + '/../database/utils.json', 'r') as f:
             extensions = json.load(f)["INITIAL_EXTENSIONS"]
 
+        lst = ""
+
         # Extensions
         for extension in extensions:
             try:
                 self.client.reload_extension(extension)
             except Exception as e:
-                print('Failed to load extension {}\n{}: {}'.format(
-                    extension, type(e).__name__, e))
+                lst += 'Falha ao recarregar {}\n{}: {}\n'.format(
+                    extension, type(e).__name__, e)
+        
+
+        await ctx.message.delete(delay = self.delete_user_message)
+        
+
+        embedmsg = embed.createEmbed(title="As extensões foram carregadas!", 
+            description= f"A extensão '{extension}' foi carregada!.",
+            color=rgb_to_int((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))),
+            fields=[
+                ("", lst, True)
+            ],
+            img="https://cdn.discordapp.com/emojis/767196592490807347.png?v=1")
+
+        # Send that shit
+        await after.send(embed=embedmsg, delete_after = self.delete_system_message)
 
 # Setup
 def setup(client):
